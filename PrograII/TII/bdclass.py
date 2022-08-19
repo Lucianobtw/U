1e626418826469f7db1e51a7dcfef33e91917a5b
MrRevillod <lrevillod2022@alu.uct.cl>
2022-08-18 21:12:35 -0400

    histclass

--- PrograII/TII/bdclass.py
+++ PrograII/TII/bdclass.py
@@ -29,34 +29,53 @@ except mysql.Error as x:
 
 def main():
     matrix = []
-    with open('student.txt', 'w') as x:
+    with open('student.txt', 'a') as x:
         for y in range(5):
             matrix.append([])
         for i in sentence:
             object = (student(i[0], i[1], i[2], i[3], i[4]))
             x.write(f'{object.id} {object.name} {object.lname} {object.email} {object.age}\n')
+            
+            #print(object)
+
             matrix[0].insert(0, object.id)
             matrix[1].insert(1, object.name)
             matrix[2].insert(2, object.lname)
             matrix[3].insert(3, object.email)
             matrix[4].insert(4, object.age)
-            
-    # print(matrix)
-    plt.subplot(3,2,1)
+
+    plt.subplot(1,5,1)
     plt.title("Id")
     plt.hist(matrix[0])
-    plt.subplot(3,2,2)
+
+    plt.subplot(1,5,2)
     plt.title("Names")
+    plt.ylim(top=1,bottom=0)
+    plt.yticks([0,1])
+    plt.xticks(rotation=90)
+    plt.xticks([1,10,20,30,40,50,60,70,80,90])
     plt.hist(matrix[1])
-    plt.subplot(3,2,3)
+
+    plt.subplot(1,5,3)
     plt.title("Last Names")
+    plt.ylim(top=1,bottom=0)
+    plt.yticks([0,1])
+    plt.xticks(rotation=90)
+    plt.xticks([1,10,20,30,40,50,60,70,80,90])
     plt.hist(matrix[2])
-    plt.subplot(3,2,4)
+    
+    plt.subplot(1,5,4)
     plt.title("Emails")
+    plt.ylim(top=1,bottom=0)
+    plt.yticks([0,1])
+    plt.xticks(rotation=90)
+    plt.xticks([1,10,20,30,40,50,60,70,80,90])
     plt.hist(matrix[3])
-    plt.subplot(3,2,5)
+
+    plt.subplot(1,5,5)
     plt.title("Ages")
     plt.hist(matrix[4])
+
     plt.show()
 
 if __name__ == "__main__":
--- web/TII/script.js
+++ web/TII/script.js
@@ -12,37 +12,9 @@ function show(id) {
     }
 } 
 
-function backgroundI(id) {
+function background(id, ruta) {
     const x = document.getElementById(id);
-    x.style.backgroundImage = "url('./img/imgI.jpg')";
-    x.style.transition = '0.5s';
-    return
-}
-
-function backgroundII(id) {
-    const x = document.getElementById(id);
-    x.style.backgroundImage = "url('./img/imgII.jpg')";
-    x.style.transition = '0.5s';
-    return
-}
-
-function backgroundIII(id) {
-    const x = document.getElementById(id);
-    x.style.backgroundImage = "url('./img/imgIII.jpg')";
-    x.style.transition = '0.5s';
-    return
-}
-
-function backgroundIV(id) {
-    const x = document.getElementById(id);
-    x.style.backgroundImage = "url('./img/imgIV.jpg')";
-    x.style.transition = '0.5s';
-    return
-}
-
-function backgroundV(id) {
-    const x = document.getElementById(id);
-    x.style.backgroundImage = "url('./img/imgV.jpg')";
+    x.style.backgroundImage = "url("+ruta+")";
     x.style.transition = '0.5s';
     return
 }
